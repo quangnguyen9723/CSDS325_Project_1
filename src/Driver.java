095b0dc2ae509059b3d06c2894b1bd5810d349aa
Quang.Ng.2003 <78969220+quangnguyen9723@users.noreply.github.com>
2023-02-18 18:58:00 -0500

    final commit

--- src/Driver.java
+++ src/Driver.java
@@ -5,23 +5,7 @@ import java.util.LinkedList;
 import java.util.ListIterator;
 
 public class Driver {
-    public static void main(String[] args) throws IOException {
-        LinkedList<Integer> list = new LinkedList<>(); {
-            list.add(1);
-            list.add(2);
-            list.add(3);
-            list.add(4);
-            list.add(5);
-        }
-
-        System.out.println(list);
-        ListIterator<Integer> li = list.listIterator();
-
-        while (li.hasNext()) {
-            int i = li.next();
-            if (i == 3) li.remove();
-        }
-
-        System.out.println(list);
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
     }
 }
